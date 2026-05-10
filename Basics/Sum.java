3306700ba78ccf0b17f81292c6debc05fedb53db
Mukund-Katare <mukundkatare682@gmail.com>
2026-05-10 23:13:22 +0530

    feat: add sum of two numbers program

---
+++ Basics/Sum.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Sum {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter 1st number: ");
+        int num1 = sc.nextInt();
+
+        System.out.print("Enter 2nd number: ");
+        int num2 = sc.nextInt();
+
+        int sum = num1 + num2;
+        System.out.println("Sum of " + num1 + " and " + num2 + " is " + sum );
+
+        sc.close();
+    }
+}
