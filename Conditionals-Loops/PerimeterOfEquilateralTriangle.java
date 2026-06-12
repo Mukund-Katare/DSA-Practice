9c62f496bea08941cb0c09a1677d2ed6fc9810f7
Mukund-Katare <mukundkatare682@gmail.com>
2026-06-12 20:10:11 +0530

    feat: add perimeter of equilateral triangle calculation

---
+++ Conditionals-Loops/PerimeterOfEquilateralTriangle.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class PerimeterOfEquilateralTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Side of Equilateral Triangle: ");
+        double side = sc.nextDouble();
+
+        double perimeter = 3 * side;
+
+        System.out.printf("Perimeter Of Equilateral Triangle is: %.2f\n", perimeter);
+
+        sc.close();
+    }
+}
