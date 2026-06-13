3a1f4aff7ccfae035f07867afc731fc264afbcd5
Mukund-Katare <mukundkatare682@gmail.com>
2026-06-13 13:50:27 +0530

    feat: add area of triangle program

---
+++ Basics/AreaOfTriangle.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class AreaOfTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Base: ");
+        double base = sc.nextDouble();
+        System.out.print("Enter Height: ");
+        double height = sc.nextDouble();
+
+        double area = 0.5 * base * height;
+
+        System.out.printf("Area of Triangle is: %.2f\n", area);
+        sc.close();
+    }
+}
